c8ec01f121b8d69058633fa4a7e1a00f98e8b1dd
WillCS <wcstibbards@gmail.com>
2018-07-27 21:50:45 +1000

    Added routes and base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { CreatePollComponent } from './create-poll/create-poll.component';
+import { PollComponent } from './poll/poll.component';
+
+const routes: Routes = [
+  {
+    path: 'poll/:id',
+    component: PollComponent
+  },
+  {
+    path: '**',
+    component: CreatePollComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatePollComponent,
+    PollComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
